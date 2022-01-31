0f183c8fbb5d5b3a2163d57e2d8857934b58e578
Renee <reneemlee92@gmail.com>
2022-01-31 14:53:59 -0800

    completed download forms

--- pinwheel.py
+++ pinwheel.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import os
 
 def get_product_info(forms):
 
@@ -22,6 +23,7 @@ def get_product_info(forms):
         soup = BeautifulSoup(response.text, "html.parser")   
 
         #html tag for the results
+        #TODO:do we need this? or can we just do find_all for the leftcellspacer etc.
         containers = soup.find_all('tr', {"class": ["odd", "even"]})
 
         lst = []
@@ -58,10 +60,56 @@ def get_product_info(forms):
         json.dump(results_list, f, indent=8, ensure_ascii=False)
     
     print("JSON file created")
-        
-        
-        
+
+def download_forms(form,beg,end):        
+    url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?"
+    
+    params = { 'value': form,
+                'criteria': 'formNumber',
+                'submitSearch': 'Find',
+                'resultsPerPage': 200,
+                'indexOfFirstRow': 0,
+                'sortColumn': 'sortOrder',
+                'isDescending':'false'
+            }
+    response = requests.get(url, params=params)
+
+    soup = BeautifulSoup(response.text, "html.parser")   
+
+    containers = soup.find_all('tr', {"class": ["odd", "even"]})
+
+    results = []
+    
+    for container in containers:
+        form_number = container.find('td', class_='LeftCellSpacer').text.strip()
+        form_link = container.find('a').get('href')
+        form_title = container.find('td', class_='MiddleCellSpacer').text.strip()
+        year = container.find('td', class_='EndCellSpacer').text.strip()
+
+        data = {'form_number': form_number,
+                'form_link': form_link,
+                'form_title' : form_title,
+                'year': int(year),
+                }
+    
+        if form == data['form_number'] and data['year'] in range(beg,end+1):
+            results.append(data)
+    
+    if results == []:
+        print("Please enter valid form number")
+    
+    current_dir = os.getcwd() 
+    new_dir = f'{current_dir}/downloads'
+    if not os.path.isdir(new_dir):
+        os.mkdir(new_dir)
+
+    for result in results:
+        file_name = f"{result['form_number']} - {result['year']}" + ".pdf"
+        file = requests.get(result['form_link'])
+
+        with open(f"{new_dir}/{file_name}", 'wb') as f:
+            f.write(file.content)    
 
 
-    
-    
+
+
