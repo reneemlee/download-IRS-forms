c82066d09790129e1a6b224d8478c8e863ce5fa9
Renee <reneemlee92@gmail.com>
2022-01-31 23:15:36 -0800

    added readme, refactored return response function

---
+++ README.txt
@@ -0,0 +1,81 @@
+FIND THE FORMS CHALLENGE
+DESCRIPTION:
+This project allows you to look up information on IRS tax forms and download them.
+
+-----
+
+TOOLS USED:
+I chose BeautifulSoup for its ease of use working with static websites.
+
+-----
+
+REFLECTION:
+In addition to fulfilling the requirements, I also added error handling in response to invalid form numbers and search dates. I also incorprated doctest and unittests to verify my outputs.
+
+Given more time, I'd like to add additional unit tests testing if given a range of valid years, my function will download all forms that exist within that range, with the earliest form revision date matching the minimum year.I'd also add corresponding error messages if the user input begin/end years fall outside of a form's revision dates.
+
+-----
+
+INSTALLATION:
+To run FIND THE FORMS on your local machine, follow the instructions below(built using Python 3.9.6):
+
+    Download and open the zipped file: pinwheel.zip
+
+    Create and activate a virtual environment inside your project directory:
+
+        $ virtualenv env (Mac OS)
+        $ virtualenv env --always-copy (Windows OS)
+        $ source env/bin/activate
+        
+    Install the dependencies: 
+        $ pip3 install -r requirements.txt
+
+-----
+
+HOW TO RUN:
+Run pinwheel.py interactively: 
+    $ python3 -i pinwheel.py
+
+To return informational results, use the get_product_info function. It takes in a list of form names as argument and returns the exact match output as json in the same format as the given instructions:
+
+** printing results to showcase formatting **
+ex: >>> print(get_product_info(["Form W-9","Form W-2"]))
+
+output:
+[
+    {
+        "form_number": "Form W-9",
+        "form_title": "Request for Taxpayer Identification Number and Certification",
+        "min_year": 1983,
+        "max_year": 2018
+    },
+    {
+        "form_number": "Form W-2",
+        "form_title": "Wage and Tax Statement (Info Copy Only)",
+        "min_year": 1954,
+        "max_year": 2022
+    }
+]
+
+To download a given tax form from range of years, use download_forms function and pass in the form name in a string, the start range of the year, then the range end. The returned forms are an exact match and will be downloaded to a subdirectory named "downloads" under the script's main directory with the name of the form as "Form Name - Year"
+
+ex: >>> download_forms("Form W-2",2000,2005)
+
+output files:
+    downloads/Form W-2 - 2000.pdf
+    downloads/Form W-2 - 2001.pdf
+    downloads/Form W-2 - 2002.pdf
+    downloads/Form W-2 - 2003.pdf
+    downloads/Form W-2 - 2004.pdf
+    downloads/Form W-2 - 2005.pdf
+
+-----
+TESTS:
+
+Run tests using:
+    $ python3 test_pinwheel.py
+
+
+
+
+
--- pinwheel.py
+++ pinwheel.py
@@ -1,7 +1,31 @@
-from bs4 import BeautifulSoup
-import requests
 import json
 import os
+import re
+import requests
+
+from bs4 import BeautifulSoup
+
+YEAR_RE = re.compile(r".*([1-3][0-9]{3})")
+
+def return_response_from_irs(form):
+    """Request from IRS"""
+
+    url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?"
+
+    params = { 'value': form,
+                'criteria': 'formNumber',
+                'submitSearch': 'Find',
+                'resultsPerPage': 200,
+                'indexOfFirstRow': 0,
+                'sortColumn': 'sortOrder',
+                'isDescending':'false'
+            }
+
+    response = requests.get(url, params=params)
+
+    response_txt = BeautifulSoup(response.text, "html.parser")   
+
+    return response_txt
 
 def get_product_info(forms):
     """Return information on list of forms
@@ -27,29 +51,15 @@ def get_product_info(forms):
         }
     ]
     """
-
-    url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?"
-
+    
     results_list = []
     
-    for item in forms:
-        params = { 'value': {item},
-                    'criteria': 'formNumber',
-                    'submitSearch': 'Find',
-                    'resultsPerPage': 200,
-                    'indexOfFirstRow': 0,
-                    'sortColumn': 'sortOrder',
-                    'isDescending':'false'
-                }
-        response = requests.get(url, params=params)
+    for form in forms:
+        search_results = return_response_from_irs(form)
 
-        soup = BeautifulSoup(response.text, "html.parser")   
+        containers = search_results.find_all('tr', {"class": ["odd", "even"]})
 
-        #html tag for the results
-        #TODO:do we need this? or can we just do find_all for the leftcellspacer etc.
-        containers = soup.find_all('tr', {"class": ["odd", "even"]})
-
-        lst = []
+        matches = []
         
         for container in containers:
             form_number = container.find('td', class_='LeftCellSpacer').text.strip()
@@ -61,13 +71,13 @@ def get_product_info(forms):
                     'year': int(year),
                     }
             #if form number matches search value, append dict to results
-            if item == data['form_number']:
-                lst.append(data)
+            if form == data['form_number']:
+                matches.append(data)
             
         #returns the min and max year of a form
         try:
-            min_year = min(lst, key=lambda k : k['year'])
-            max_year = max(lst, key=lambda k : k['year'])
+            min_year = min(matches, key=lambda k : k['year'])
+            max_year = max(matches, key=lambda k : k['year'])
             
             results = {'form_number': max_year['form_number'],
                         'form_title' : max_year['form_title'],
@@ -77,30 +87,25 @@ def get_product_info(forms):
             
             results_list.append(results)
         except:
-            print(f'{item} does not exist. Please enter valid search')
+            print(f'{form} does not exist. Please enter valid search')
         
-        if results_list != []:
-            results_json = json.dumps(results_list, indent=4)
-            return results_json
+    if results_list != []:
+        results_json = json.dumps(results_list, indent=4)
+        return results_json
 
 def download_forms(form,beg,end):
-    """Download available forms within a given range"""        
+    """Download available forms within a given range""" 
 
-    url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html?"
-    
-    params = { 'value': form,
-                'criteria': 'formNumber',
-                'submitSearch': 'Find',
-                'resultsPerPage': 200,
-                'indexOfFirstRow': 0,
-                'sortColumn': 'sortOrder',
-                'isDescending':'false'
-            }
-    response = requests.get(url, params=params)
+    check_beg = re.search(YEAR_RE,str(beg))
+    check_end = re.search(YEAR_RE,str(end))
 
-    soup = BeautifulSoup(response.text, "html.parser")   
+    if not check_beg and check_end:
+         print("Please enter valid year range")
+         quit()
+         
+    search_results = return_response_from_irs(form)       
 
-    containers = soup.find_all('tr', {"class": ["odd", "even"]})
+    containers = search_results.find_all('tr', {"class": ["odd", "even"]})
 
     results = []
     
@@ -122,6 +127,7 @@ def download_forms(form,beg,end):
     if results == []:
         print("Please enter valid form number")
     
+    #create subdirectory
     current_dir = os.getcwd() 
     new_dir = f'{current_dir}/downloads'
     if not os.path.isdir(new_dir):
@@ -132,7 +138,9 @@ def download_forms(form,beg,end):
         file = requests.get(result['form_link'])
 
         with open(f"{new_dir}/{file_name}", 'wb') as f:
-            f.write(file.content)    
+            f.write(file.content) 
+
+
 
 
 
